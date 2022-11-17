c08fceafcb3d430ca1114682ef96255a07b65142
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2022-11-16 21:31:20 -0300

    links bugados corrigidos

--- main.py
+++ main.py
@@ -4,17 +4,18 @@ from bs4 import BeautifulSoup
 import time
 
 navegador = "firefox"
-link = 'https://aniyan.net/'
+link = 'https://www.narutogame.com.br/'
 webDriver = Driver(navegador, root='drivers').driver
 
 
 webDriver.get(link)
-# time.sleep(2)
+time.sleep(5)
 page:str = webDriver.page_source
 webDriver.close()
 
 # tratamento da pagina
-page = page.replace('src="/', 'src="'+link).replace('href="/', 'href="'+link)
+page = page.replace('src="', 'src="'+link).replace('href="', 'href="'+link).replace(link+link, link)
+
 with open("page.html", "wb",) as f:
     f.write(page.encode("utf-8"))
     f.close()
